24f7f1cf181ea1b3cebb2036ed6d67db811699a4
Alan Vieceli <alansvieceli@gmail.com>
2019-06-24 20:18:59 -0300

    Aplicando CORS

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,7 @@
 const port = 3003
 
+const allowCors = require("./cors");
+
 //Parser do corpo da requisição, Formulario, JSON, XML
 const bodyParser = require("body-parser");
 //Servidor web que roda em cima do node
@@ -8,6 +10,7 @@ const server = express();
 
 server.use(bodyParser.urlencoded({extended: true})); //formulario
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function(){
     console.log(`BACKEND está rodando na porta ${port}, quiridu!!`)
