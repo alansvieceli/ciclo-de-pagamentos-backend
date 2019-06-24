26e9f5b0e5f25a4a7cd59a27b154fb27cd622362
Alan Vieceli <alansvieceli@gmail.com>
2019-06-24 19:30:33 -0300

    Obtendo a Qtde de Registros (Count)

--- src/api/ciclo-de-pagamentos/ciclo-de-pagamentos-service.js
+++ src/api/ciclo-de-pagamentos/ciclo-de-pagamentos-service.js
@@ -2,6 +2,16 @@ const CicloPagamento = require('./ciclo-de-pagamentos');
 
 CicloPagamento.methods(['get', 'post', 'put', 'delete']);
 CicloPagamento.updateOptions({new: true, runValidators: true});
-CicloPagamento.ins
+
+CicloPagamento.route('count', (req, res, next) => {
+    CicloPagamento.count((error, value) => {
+        if (error){
+            res.status(500).json({errors: [error]});
+        } else {
+            res.json({value})
+        }
+    });
+});
+
 
 module.exports = CicloPagamento;
