18fd5750341bcea641b1396462239d3011aa3369
Alan Vieceli <alansvieceli@gmail.com>
2019-07-23 21:37:50 -0300

    Implementando autenticação

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,6 +3,6 @@
 module.exports = function(request, response, next) {
     response.header('Access-Control-Allow-Origin', '*'); //poderia listar os ips
     response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 }
