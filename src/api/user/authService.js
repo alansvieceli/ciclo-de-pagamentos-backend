6076fb752623591e8912faaf46bbf4959346431e
Alan Vieceli <alansvieceli@gmail.com>
2019-08-01 09:34:57 -0300

    ajuste na geração do token

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -22,17 +22,19 @@ const login = (req, res, next) => {
         if (err) {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user, env.authSecret, {
+            const token = jwt.sign(user.toJSON(), env.authSecret, {
                 expiresIn: "1 day"
             })
             const { name, email } = user
-            res.json({ name, email, token })
+            res.json({ name, email, token })            
         } else {
             return res.status(400).send({
                 errors: ['Usuário/Senha inválidos']
             })
         }
     })
+  
+
 }
 
 const validateToken = (req, res, next) => {
